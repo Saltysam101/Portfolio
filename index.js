202addd0fe77fc9630a9ee2c6f1271fc60a05beb
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2020-07-01 10:01:36 -0400

    styled home page for mobile devices

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
 const body = document.querySelector(".body");
 
 
